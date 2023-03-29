4315ad6ec7122008e79a108b6c2e70cead1fddc9
Jules Bouédec <jules.bouedec.pro@gmail.com>
2023-03-29 07:14:57 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jules Bouédec on 29/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
